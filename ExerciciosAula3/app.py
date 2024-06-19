48b8e20318dc053dadd0d10fe3d6040dd07afb5e
Danillo <danillordm@gmail.com>
2024-06-19 12:56:11 -0300

    Adiciona ExerciciosAula3

---
+++ ExerciciosAula3/app.py
@@ -0,0 +1,22 @@
+# 1. Crie duas variáveis a e b com valores numéricos quaisquer. 
+# Em seguida, calcule e imprima a soma desses dois números.
+a = 42
+b = 73
+print(f'A soma entre {a} e {b} é {a + b}')
+
+# 2. Defina duas variáveis x e y com valores decimais. 
+# Calcule e imprima o resultado da multiplicação entre esses dois números.
+x = 39.123
+y = 89.21
+print(f'O resultado da multiplicação entre {x} e {y} é {x * y}')
+
+# 3. Defina uma variável valor com um número inteiro. 
+# Em seguida, calcule e imprima o resultado da divisão desse número por 3.
+valor = int(input('Digite um número inteiro: '))
+print(f'o resultado da divisão de {valor} por 3 é {valor / 3}')
+
+# 4. Crie duas variáveis frase1 e frase2 com suas próprias frases curtas. 
+# Concatene essas duas variáveis para formar uma nova frase e imprima-a.
+frase1 = input('Digite a primeira frase: ')
+frase2 = input('Digite a segunda frase: ')
+print(frase1,frase2)
